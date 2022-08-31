364f1607456b9b7af381c32da32bb76af9b30226
Emily Wise <wiseemily88@github.com>
2022-08-31 14:13:05 -0700

    Update nat_test.go

--- nat_test.go
+++ nat_test.go
@@ -1,4 +1,4 @@
 func TestNetworkLoopbackNat(t *testing.T) {
-	skip.If(t, testEnv.GitHubActions) // https://github.com/moby/moby/issues/41561
+	skip.If(t, testEnv.GitHubActions) //https://github.com/wiseemily88/Testing-prod-repo/issues/9
 	skip.If(t, testEnv.OSType == "windows", "FIXME")
 	skip.If(t, testEnv.IsRemoteDaemon)
